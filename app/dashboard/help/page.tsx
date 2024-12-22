6aa783ae2451bc37e5700923009d69058d36e1a0
Vasily Gusev <vasily_gusev@epam.com>
2024-12-22 16:55:03 +0300

    Chapter 10 - PPR switched on

--- app/dashboard/help/page.tsx
+++ app/dashboard/help/page.tsx
@@ -1,5 +1,14 @@
 import { inter } from '@/app/ui/fonts';
 
+import css from './help.module.css';
+
 export default function Page() {
-  return <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>Help Page</h1>;
+  return (
+  <>
+    <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>Help Page</h1>
+    <div
+      className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+    />
+    <div className={css.shape} />
+  </>);
 }
